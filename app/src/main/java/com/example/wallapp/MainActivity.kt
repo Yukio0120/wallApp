16b73eba62fcd7bde2b93ff43a849406c347faf1
Lyan <dmitlyan@yandex.ru>
2022-05-29 00:28:46 +0300

    ddd

--- app/src/main/java/com/example/wallapp/MainActivity.kt
+++ app/src/main/java/com/example/wallapp/MainActivity.kt
@@ -5,25 +5,39 @@ import android.app.WallpaperManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-
+import kotlinx.android.synthetic.main.item.*
 
 
 class MainActivity : AppCompatActivity() {
+
+
+
+
     @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        img_wallpaper.setImageResource(R.drawable.one_img)
-        img_wallpaper.setImageResource(R.drawable.img_two)
+
+        val imgs = listOf<Int>(R.drawable.one_img, R.drawable.img_two)
+        val adapter = Adapter(imgs, this)
+        pager.adapter = adapter
+
         set_up.setOnClickListener {
             val imgManager: WallpaperManager = WallpaperManager.getInstance(this)
             imgManager.setResource(R.drawable.one_img)
-            imgManager.setResource(R.drawable.img_two)
         }
+
     }
 }
 
 
-
-
+//    img_wallpaper.setImageResource(R.drawable.one_img)
+//    img_wallpaper.setImageResource(R.drawable.img_two)
+//    set_up.setOnClickListener {
+//        val imgManager: WallpaperManager = WallpaperManager.getInstance(this)
+//        imgManager.setResource(R.drawable.one_img)
+//        imgManager.setResource(R.drawable.img_two)
+//    }
+//}
 
